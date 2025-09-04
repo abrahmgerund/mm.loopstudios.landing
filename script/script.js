24a427efe89d378911e9a8f1272f6d38ca677279
abrahmgerund <abrahmgerund@gmail.com>
2025-09-03 20:09:11 -0600

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,29 @@
+const hamburgerBtn = document.querySelector('.nav__mobile-nav-hamburger-btn');
+const hamburgerClose = document.querySelector('.nav__mobile-nav-hamburger-close');
+const hamburgerSlide = document.querySelector('.nav__mobile-nav-slide');
+const darkOverlay = document.querySelector('.dark-overlay');
+const nav = document.querySelector('.nav')
+
+const toggleSlide = function () {
+    hamburgerBtn.classList.toggle('nav__mobile-nav-hamburger-btn--active');
+    hamburgerClose.classList.toggle('nav__mobile-nav-hamburger-close--active');
+    hamburgerSlide.classList.toggle('nav__mobile-nav-slide--active');
+    darkOverlay.classList.toggle('dark-overlay--active');
+}
+
+const toggleNavFilter = function () {
+    if (window.scrollY > 0) {
+        nav.classList.add('nav--scroll-active');
+    } else {
+        nav.classList.remove('nav--scroll-active');
+    }
+
+}
+
+
+
+
+
+hamburgerBtn.addEventListener('click', toggleSlide);
+hamburgerClose.addEventListener('click', toggleSlide);
+window.addEventListener('scroll', toggleNavFilter);
