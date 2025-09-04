8ccd89f59516d93c9f2621fb28c5e6aad5a27968
abrahmgerund <abrahmgerund@gmail.com>
2025-09-04 14:32:58 -0600

    responsiveness update

--- script/script.js
+++ script/script.js
@@ -2,13 +2,17 @@ const hamburgerBtn = document.querySelector('.nav__mobile-nav-hamburger-btn');
 const hamburgerClose = document.querySelector('.nav__mobile-nav-hamburger-close');
 const hamburgerSlide = document.querySelector('.nav__mobile-nav-slide');
 const darkOverlay = document.querySelector('.dark-overlay');
-const nav = document.querySelector('.nav')
+const nav = document.querySelector('.nav');
+const links = document.querySelectorAll('a');
+
 
 const toggleSlide = function () {
     hamburgerBtn.classList.toggle('nav__mobile-nav-hamburger-btn--active');
     hamburgerClose.classList.toggle('nav__mobile-nav-hamburger-close--active');
     hamburgerSlide.classList.toggle('nav__mobile-nav-slide--active');
     darkOverlay.classList.toggle('dark-overlay--active');
+
+    links.forEach((link) => link.addEventListener('click', toggleSlide));
 }
 
 const toggleNavFilter = function () {
@@ -27,3 +31,6 @@ const toggleNavFilter = function () {
 hamburgerBtn.addEventListener('click', toggleSlide);
 hamburgerClose.addEventListener('click', toggleSlide);
 window.addEventListener('scroll', toggleNavFilter);
+
+
+console.log(links);
